85cc50e902c60a1418e0c34c8a69362cb6ab721d
Arlony Diaz <16887430+rlon23@users.noreply.github.com>
2022-07-29 13:35:47 -0500

    initial commit

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Error = () => {
+  return (
+    <main>
+      <section>
+        <h1>404</h1>
+        <h3>page not found</h3>
+      </section>
+    </main>
+  )
+}
+
+export default Error
