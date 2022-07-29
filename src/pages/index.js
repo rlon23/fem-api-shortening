aa714d73847e475921c860cc28c8272530a58682
Arlony Diaz <16887430+rlon23@users.noreply.github.com>
2022-07-29 18:45:27 -0500

    add footer mobile.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import Layout from "./../components/Layout"
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <Layout>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </Layout>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/pages/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      "dark-violet": "#3B3054",
+      "very-dark-violet": "#232127",
+      cyan: "#2ACFCF",
+      "cyan-hover": "#9CE2E4",
+      "red-error": "#F46262",
+      gray: "#BFBFBF",
+      "grayish-violet": "#9E9AA7",
+      white: "#FFFFFF",
+      black: "000000",
+      "off-white": "#F0F1F6",
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["'Poppins', sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
