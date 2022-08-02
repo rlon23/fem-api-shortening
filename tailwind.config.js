bf44ac1dfcc665298f5290130cebb2396a187c7c
Arlony Diaz <16887430+rlon23@users.noreply.github.com>
2022-08-02 11:41:23 -0500

    add fill variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,jsx,ts,tsx}',
-    './src/components/**/*.{js,jsx,ts,tsx}',
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
@@ -10,17 +10,20 @@ module.exports = {
         poppins: ["'Poppins', sans-serif"],
       },
       colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        'dark-violet': '#3B3054',
-        'very-dark-violet': '#232127',
-        'main-cyan': '#2ACFCF',
-        'cyan-hover': '#9CE2E4',
-        'red-error': '#F46262',
-        'main-gray': '#BFBFBF',
-        'grayish-violet': '#9E9AA7',
-        'off-white': '#F0F1F6',
+        transparent: "transparent",
+        current: "currentColor",
+        "dark-violet": "#3B3054",
+        "very-dark-violet": "#232127",
+        "main-cyan": "#2ACFCF",
+        "cyan-hover": "#9CE2E4",
+        "red-error": "#F46262",
+        "main-gray": "#BFBFBF",
+        "grayish-violet": "#9E9AA7",
+        "off-white": "#F0F1F6",
       },
+    },
+    variants: {
+      fill: ["hover", "focus"],
     },
   },
   plugins: [],
